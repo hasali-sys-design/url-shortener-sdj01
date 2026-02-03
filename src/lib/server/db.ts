6d9b056ddf3b343cf77a4de3085805b95f62c4ed
hasali <hali28.ent@gmail.com>
2026-02-03 14:57:27 -0500

    DB connection setup

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,12 +1,7 @@
 import postgres from 'postgres'
-const sql = postgres('postgres://username:password@host:port/database',{
-    host: '127.0.0.1' ,
-    port: 5433,
-    database: 'urldb',
-    username: 'postgres',
-    password: 'pg1016'
 
+const connectionString = process.env.DATABASE_URL ?? 'postgres://postgres:pg1016@127.0.0.1:5433/urldb'
 
-})
+const sql = postgres(connectionString)
 
 export default sql
