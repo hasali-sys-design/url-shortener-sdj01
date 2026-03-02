524892194210dd3e31d9aef7a1565aa76a4825d4
hasali <hali28.ent@gmail.com>
2026-03-02 17:49:34 -0500

    Fixed redis error due to ttl timing being non positive integer. Fixed pm2 issue with 500 internal error. Everything works, pm2 now gives < 5ms latency with 30+k req/s

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
     env: {
       NODE_ENV: 'production',
       PORT: 3000,
-      DATABASE_URL:'postgresql://[postgres[:pg1016]@]localhost[:5433][/urldb]]',
+      DATABASE_URL:'postgresql://postgres:pg1016@localhost:5433/urldb',
       REDIS_URL: 'redis://localhost:6379',
       ORIGIN: 'http://localhost:3000' // Required for production
     }
--- src/routes/[short]/+server.ts
+++ src/routes/[short]/+server.ts
@@ -1,25 +1,27 @@
-import { error, redirect } from '@sveltejs/kit'
-import sql from '$lib/server/db'
-import redis from '$lib/server/redis'
+import { error, redirect } from "@sveltejs/kit";
+import sql from "$lib/server/db";
+import redis from "$lib/server/redis";
 
 export const GET = async ({ params }) => {
-   
-    const cachedKey = `url:${params.short}`
-    const cachedUrl = await redis.get(cachedKey)
-    if(cachedUrl)
-        throw redirect(302, cachedUrl)
-    
-	const [row] = await sql`
-    SELECT long_url
-    FROM urlmappings
-    WHERE short_url = ${params.short}`
+    const cachedKey = `url:${params.short}`;
+    const cachedUrl = await redis.get(cachedKey);
+    if (cachedUrl) throw redirect(302, cachedUrl);
+
+    const [row] = await sql`
+        SELECT 
+            long_url,
+            EXTRACT(EPOCH FROM (expiry_time - NOW())) AS ttl 
+        FROM urlmappings
+        WHERE short_url = ${params.short}
+    `;
 
     if (!row) {
-        throw error(404, 'Short URL not found')
+        throw error(404, "Short URL not found");
     }
 
-    const ttlSeconds = row.expiry_time - Date.now()
-    await redis.set(cachedKey, row.long_url, {EX: ttlSeconds })
-    
-    throw redirect(302, row.long_url)
+    if (row.ttl > 0) {
+        await redis.set(cachedKey, row.long_url, { EX: Math.floor(row.ttl) });
+    }
+
+    throw redirect(302, row.long_url);
 };
